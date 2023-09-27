87cc7db535d8c059055bbb2c375f83bab4b89353
hugo <hgk9872@gmail.com>
2023-09-27 19:27:29 +0900

    gitignore 설정

--- src/main/java/com/modu/ClientViewServer/HomeController.java
+++ src/main/java/com/modu/ClientViewServer/HomeController.java
@@ -1,22 +1,13 @@
 package com.modu.ClientViewServer;
 
-import com.modu.ClientViewServer.chat.ChatMessageDto;
-import com.modu.ClientViewServer.chat.ChatRoomDto;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.UUID;
 
 @Controller
 @RequiredArgsConstructor
--- src/main/java/com/modu/ClientViewServer/chat/ChatController.java
+++ src/main/java/com/modu/ClientViewServer/chat/ChatController.java
@@ -8,7 +8,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
